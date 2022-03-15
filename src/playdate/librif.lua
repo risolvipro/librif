1817f70ced3b77b49fcf46fb3cd5d041d0b07063
Risolvi Productions <risolvi.dev@gmail.com>
2022-03-15 01:51:09 +0100

    Lua fix

--- src/playdate/librif.lua
+++ src/playdate/librif.lua
@@ -1,3 +1,3 @@
 librif.graphics.kDitherTypeBayer2 = 0
-librif.graphics.kDitherTypeBayer4 = 0
-librif.graphics.kDitherTypeBayer8 = 0
+librif.graphics.kDitherTypeBayer4 = 1
+librif.graphics.kDitherTypeBayer8 = 2
